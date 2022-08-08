002425d27304bdc31322499d04ea998180859d79
pohuliaieva <bratkevich.e.a@gmail.com>
2022-08-08 02:20:51 +0100

    Add search city function

--- scr/script.js
+++ scr/script.js
@@ -34,8 +34,21 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", `${response.data.weather[0].description}`);
 }
 
+function search(city) {
+    let apiKey = "49813f7b6218c304bf646ff9c4c866c4";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "49813f7b6218c304bf646ff9c4c866c4";
-let city = "Madrid";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+
+
+function handleSubmit(event) {
+    event.preventDefault()
+    let cityInputElement = document.getElementById("city-input");
+    search(cityInputElement.value);
+}
+
+search("Lisbon");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
